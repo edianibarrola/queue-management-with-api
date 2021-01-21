274bc1da2a44b1972946a355d51d330da24b5077
edianibarrola <edianibarrola@gmail.com>
2021-01-21 17:56:39 +0000

    Finished

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from sms import send
+from datastructures import Queue
 #from models import Person
 
 app = Flask(__name__)
@@ -19,7 +21,7 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
+queue=Queue()
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -32,12 +34,38 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Client Added ",
+        "resp": queue.get_queue()
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def handle_enqueue():
+    newClient = request.get_json()
+    queue.enqueue(newClient)
+    if newClient is None:
+            return "The enqueue request body is null", 400
+    response_body = {
+        "enqueued": newClient,
+        "resp": queue.get_queue()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/userdequeue', methods=['DELETE'])
+def handle_dequeue():
+    
+    user=queue.dequeue()
+    send(body='',to=user['phone'] )
+    if user is None:
+        return "The dequeue request body is null", 400
+    response_body = {
+        "dequeued": user,
+        "resp": queue.get_queue()
+    }
+    return jsonify(response_body),200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
---
+++ src/sms.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+def send(body='',to=''):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body='Your table is ready! Bon Apetit!',
+        from_='+19082244180',
+        to='+19543090566'
+    )
+
+    print(message.sid)
